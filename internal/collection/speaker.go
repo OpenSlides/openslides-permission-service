a948638b6c3091d6a60a6183356496dd3efc783b
Oskar Hahn <mail@oshahn.de>
2020-12-24 09:07:25 +0100

    Speaker.read

--- internal/collection/speaker.go
+++ internal/collection/speaker.go
@@ -24,6 +24,7 @@ func NewSpeaker(dp dataprovider.DataProvider) *Speaker {
 // Connect connects the list_of_speakers routes.
 func (sp *Speaker) Connect(s perm.HandlerStore) {
 	s.RegisterWriteHandler("speaker.delete", perm.WriteCheckerFunc(sp.delete))
+	s.RegisterReadHandler("speaker", perm.ReadeCheckerFunc(sp.read))
 
 }
 
@@ -51,4 +52,31 @@ func (sp *Speaker) delete(ctx context.Context, userID int, payload map[string]js
 	}
 
 	return nil, nil
+}
+
+func (sp *Speaker) read(ctx context.Context, userID int, fqfields []perm.FQField, result map[string]bool) error {
+	var hasPerm bool
+	var lastID int
+	for _, fqfield := range fqfields {
+		if lastID != fqfield.ID {
+			hasPerm = sp.hasReadPerm(ctx, userID, fqfield)
+		}
+		if hasPerm {
+			result[fqfield.String()] = true
+		}
+	}
+	return nil
+}
+
+func (sp *Speaker) hasReadPerm(ctx context.Context, userID int, fqfield perm.FQField) bool {
+	fqid := fmt.Sprintf("speaker/%d", fqfield.ID)
+	meetingID, err := sp.dp.MeetingFromModel(ctx, fqid)
+	if err != nil {
+		return false
+	}
+
+	if err := perm.EnsurePerms(ctx, sp.dp, userID, meetingID, "agenda.can_see_list_of_speakers"); err != nil {
+		return false
+	}
+	return true
 }
